033210652c7c97966d90d5e379bcf1e82e9561e3
Alla199707 <onoschenko20@gmail.com>
2023-05-27 22:10:56 +0300

    исправление багов

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { RestrictedRoute } from './RestricredRoute';
 import { PrivateRoute } from './PrivateRoute';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from 'redux/auth/authOperations';
 
 const HomePage = lazy(() => import('../pages/Home/HomePage'));
 const LoginPage = lazy(() => import('../pages/Login/Login'));
 const RegisterPage = lazy(() => import('../pages/Register/Register'));
 const ContactsPage = lazy(() => import('../pages/Contacts/ContactsPage'));
 export const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
